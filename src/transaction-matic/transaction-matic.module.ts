c18693b71d9c79db723b730ca0ce62e604530a04
jondanny <jdnguyen928@gmail.com>
2022-12-07 09:38:01 +0100

    Fix dependency issues

--- src/transaction-matic/transaction-matic.module.ts
+++ src/transaction-matic/transaction-matic.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TransactionMaticService } from './transaction-matic.service';
 import { TransactionMaticController } from './transaction-matic.controller';
+import { TransactionMaticRepository } from './transaction-matic.repository';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TransactionMatic } from './transaction-matic.entity';
 
 @Module({
-  providers: [TransactionMaticService],
+  imports: [TypeOrmModule.forFeature([TransactionMatic])],
+  providers: [TransactionMaticService, TransactionMaticRepository],
   controllers: [TransactionMaticController],
+  exports: [TransactionMaticService],
 })
 export class TransactionMaticModule {}
--- src/transaction-nft/transaction-nft.module.ts
+++ src/transaction-nft/transaction-nft.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TransactionNftService } from './transaction-nft.service';
 import { TransactionNftController } from './transaction-nft.controller';
+import { TransactionNftRepository } from './transaction-nft.repository';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TransactionNft } from './transaction-nft.entity';
 
 @Module({
-  providers: [TransactionNftService],
+  imports: [TypeOrmModule.forFeature([TransactionNft])],
+  providers: [TransactionNftService, TransactionNftRepository],
   controllers: [TransactionNftController],
+  exports: [TransactionNftService],
 })
 export class TransactionNftModule {}
