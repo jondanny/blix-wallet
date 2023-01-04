358505c1276415b9a3f6fab4329166d49cbb0f6a
jondanny <jdnguyen928@gmail.com>
2023-01-04 20:13:19 +0400

    rename

--- hardhat/config/contracts.ts
+++ hardhat/config/contracts.ts
@@ -1,2 +1,2 @@
-export const nftTestContractAddress = '0x0a2f4BA5F83b3303417D8148a161A074f2e908B7';
+export const nftContractAddress = '0x0a2f4BA5F83b3303417D8148a161A074f2e908B7';
 export const adminListContractAddress = '0x4B10679026491061ef52491f52Ab3574f0c51E09';
